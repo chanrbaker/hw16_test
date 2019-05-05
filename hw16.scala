f231c62a3defa23c70d7b8d987573823a1a0c5b3
chanrbaker <chan1196@gmail.com>
2019-05-05 15:17:44 -0400

    Fixed big0 for Q1&2

--- hw16.scala
+++ hw16.scala
@@ -5,7 +5,7 @@ object hw16 extends eecs.cs385 {
   // REMEMBER THAT ignoretest MUST BE SET ON AT LEAST
   // TWO PROBLEMS WHEN YOU'RE DONE.
 
-  // *** insert running time here *** O(n^4)
+  // *** O(n^4) ***
   def albertsShare(loot: List[Int]): Int = {
 
     val n = loot.length
@@ -88,14 +88,28 @@ object hw16 extends eecs.cs385 {
   ignoretest("albertsShare", albertsShare _, "loot")
 
 
-  // *** insert running time here ***
+  // *** O(nlogn) ***
+  //
+  // The first for loop runs in nlogn
+  //
+  // The second for loop goes through at worst n/2 Sets, where each Set is size 1
+  // because we can choose every other number.  In this case, .size is constant, so
+  // it runs in linear time for n/2, which we can reduce to n.
+  //
+  // If we have one large group, the for loop will run in constant time, and the Set
+  // will have n-1 elements, making .size linear.  It will run in n-1 time, or just n
+  //
+  // If there is one choice taken in the middle, we will have 2 Sets of (n/2)-1
+  // so the for loop will run twice and each .size will run in (n/2)-1, which is also
+  // linear.
   // SERIOUS BUG COUNT = 3
   def lotteryPick(n: Int, taken: List[Int]): Int = {
     var groups = List[Set[Int]]()
+    val take2 = taken.toSet
 
     var s = Set[Int]()
     for (i <- 1 to n){
-      if (!taken.contains(i)){
+      if (!take2.contains(i)){
         s += i
         if (i == n) groups = s+:groups
       }
